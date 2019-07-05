7bc65c1b9b88ba4066673473df57cb43bd5fdc5f
Liam White <liam@tetrate.io>
2019-07-05 11:26:02 +0100

    Add initial run for local files
    Signed-off-by: Liam White <liam@tetrate.io>

---
+++ pkg/binary/run.go
@@ -0,0 +1,31 @@
+// Copyright 2019 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package binary
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func Run(path string, args []string) error {
+	if _, err := os.Stat(path); err != nil {
+		return fmt.Errorf("unable to stat %q: %v", path, err)
+	}
+	if err := syscall.Exec(path, append([]string{"envoy"}, args...), os.Environ()); err != nil {
+		return fmt.Errorf("unable to exec %q: %v", path, err)
+	}
+	return nil
+}
