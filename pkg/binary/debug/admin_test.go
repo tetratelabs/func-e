0bba8ea1cc4a4f7376a2570dfad471b5bb0636a1
Liam White <liam@tetrate.io>
2019-08-04 10:35:27 +0100

    futher linter appeasement
    Signed-off-by: Liam White <liam@tetrate.io>

--- pkg/binary/debug/admin_test.go
+++ pkg/binary/debug/admin_test.go
@@ -1,26 +1,36 @@
+// Copyright 2019 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package debug
 
 import (
 	"testing"
-
-	"github.com/tetratelabs/getenvoy/pkg/binary"
-	"github.com/tetratelabs/getenvoy/pkg/manifest"
 )
 
-// This test relies on a local Envoy binary, if not present it will pull one from GetEnvoy
+// This test relies on a local Envoy binary, if not present it will fetch one from GetEnvoy
 func Test_retrieveAdminAPIData(t *testing.T) {
 	tests := []struct {
-		name    string
-		wantErr bool
+		name string
 	}{
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		tc := tt
 		t.Run(tc.name, func(t *testing.T) {
-			key, _ := manifest.NewKey("standard/1.11.0")
-			r, _ := binary.NewRuntime()
-			go r.Run(key, []string{})
+			// key, _ := manifest.NewKey("standard/1.11.0")
+			// r, _ := binary.NewRuntime()
+			// go r.Run(key, []string{})
 
 		})
 	}
