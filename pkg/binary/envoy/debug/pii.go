13f472fba8241d3de21e1610b14a668010d8fe37
Hengyuan <jhy8890948@gmail.com>
2019-09-10 10:51:46 -0700

    add function that filters logs by format string and hashes PII fields of the valid logs
    Signed-off-by: Hengyuan <jhy8890948@gmail.com>

---
+++ pkg/binary/envoy/debug/pii.go
@@ -0,0 +1,48 @@
+package debug
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"bitbucket.org/creachadair/shell"
+)
+
+// process the array of logs by filtering it using format and containsPII, Hash the resulting array of logs and return the final array.
+// an empty array of strings and an error instance will be returned in the event of an error
+func Process_modified(logs []string, format string, containsPII map[string]bool) ([]string, error) {
+	// filter the valid logs according to the format str
+	fieldNames, ok := shell.Split(format)
+	if !ok {
+		return []string{}, fmt.Errorf("error in splitting format string: %s", format)
+	}
+
+	out := make([]string, 0, len(logs))
+	for _, log := range logs {
+		fieldValues, ok := shell.Split(log)
+		if !ok {
+			return []string{}, fmt.Errorf("error in splitting log: %s", log)
+		}
+
+		if len(fieldValues) == len(fieldNames) {
+			// pick the PII fields and Hash the fields
+			for j, name := range fieldNames {
+				if containsPII[name] {
+					hash, err := Hash(fieldValues[j])
+					if err != nil {
+						return []string{}, fmt.Errorf("error in hashing the field: %s", fieldValues[j])
+					}
+					fieldValues[j] = hash
+				}
+			}
+			out = append(out, shell.Join(fieldValues))
+		}
+	}
+	return out, nil
+}
+
+// TODO: salt the Hash
+func Hash(s string) (string, error) {
+	h := sha256.New()
+	h.Write([]byte(s)) //nolint: errcheck
+	return string(h.Sum(nil)), nil
+}
