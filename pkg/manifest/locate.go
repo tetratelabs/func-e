7c074165fa333f75be434c1b6c16f9ab07bec005
Hengyuan <jhy8890948@gmail.com>
2019-12-09 18:48:13 -0800

    expose LocateBuild function for manifest validation
    Signed-off-by: Hengyuan <jhy8890948@gmail.com>

--- pkg/manifest/locate.go
+++ pkg/manifest/locate.go
@@ -87,10 +87,10 @@ func Locate(key *Key, manifestLocation string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return locateBuild(key, manifest)
+	return LocateBuild(key, manifest)
 }
 
-func locateBuild(key *Key, manifest *api.Manifest) (string, error) {
+func LocateBuild(key *Key, manifest *api.Manifest) (string, error) {
 	// This is pretty horrible... Not sure there is a nicer way though.
 	if manifest.Flavors[key.Flavor] != nil && manifest.Flavors[key.Flavor].Versions[key.Version] != nil {
 		for _, build := range manifest.Flavors[key.Flavor].Versions[key.Version].Builds {
