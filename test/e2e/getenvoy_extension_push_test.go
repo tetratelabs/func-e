6215f127e22bc56fb85ed8966ebdfa8437d4e460
Kotaro Inoue <k.musaino@gmail.com>
2021-01-28 20:39:52 +0900

    Fix e2e test to run envoy with pulled image
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- test/e2e/getenvoy_extension_push_test.go
+++ test/e2e/getenvoy_extension_push_test.go
@@ -15,8 +15,18 @@
 package e2e_test
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sync"
+	"time"
+
+	"github.com/tetratelabs/getenvoy/pkg/common"
+	utilenvoy "github.com/tetratelabs/getenvoy/test/e2e/util/envoy"
+
+	"github.com/tetratelabs/getenvoy/pkg/extension/wasmimage"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
@@ -29,10 +39,46 @@ import (
 )
 
 const (
-	localRegistryWasmImageRef = "localhost:5000/getenvoy/sample"
+	localRegistryWasmImageRef = "localhost:5000/getenvoy/sample:latest"
 )
 
 var _ = Describe("getenvoy extension push", func() {
+	var debugDir string
+
+	BeforeEach(func() {
+		debugDir = filepath.Join(common.DefaultHomeDir(), "debug")
+	})
+
+	var backupDebugDir string
+
+	BeforeEach(func() {
+		_, err := ioutil.ReadDir(debugDir)
+		if os.IsNotExist(err) {
+			return
+		}
+		Expect(err).NotTo(HaveOccurred())
+
+		By("backing up GetEnvoy debug dir")
+		backupDir, err := ioutil.TempDir(filepath.Dir(debugDir), "debug")
+		Expect(err).NotTo(HaveOccurred())
+		err = os.RemoveAll(backupDir)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = os.Rename(debugDir, backupDir)
+		Expect(err).NotTo(HaveOccurred())
+		backupDebugDir = backupDir
+	})
+
+	AfterEach(func() {
+		if backupDebugDir == "" {
+			return
+		}
+		By("restoring GetEnvoy debug dir from backup")
+		err := os.RemoveAll(debugDir)
+		Expect(err).NotTo(HaveOccurred())
+		err = os.Rename(backupDebugDir, debugDir)
+		Expect(err).NotTo(HaveOccurred())
+	})
 
 	type testCase e2e.CategoryLanguageTuple
 
@@ -44,9 +90,18 @@ var _ = Describe("getenvoy extension push", func() {
 		return testCases
 	}
 
+	AtMostOnce := func(fn func()) func() {
+		var once sync.Once
+		return func() {
+			once.Do(fn)
+		}
+	}
+
 	// TODO(musaprg): write teardown process for local registries if it's needed
 
 	const extensionName = "my.extension"
+
+	const terminateTimeout = 2 * time.Minute
 
 	DescribeTable("should push a *.wasm file",
 		func(given testCase) {
@@ -90,6 +145,78 @@ var _ = Describe("getenvoy extension push", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(stdout).NotTo(BeEmpty())
 			Expect(stderr).To(BeEmpty())
+
+			By("pulling pushed wasm binary")
+			puller, err := wasmimage.NewPuller(false, false)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(puller).NotTo(BeNil())
+			dstPath := filepath.Join(outputDir, "pulled_extension.wasm")
+			_, err = puller.Pull(localRegistryWasmImageRef, dstPath)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("running `extension run` command with pulled image")
+			_, se, cancel, errs := GetEnvoy("extension run").
+				Args(e2e.Env.GetBuiltinContainerOptions()...).
+				Arg("--extension-file").Arg(dstPath).
+				Start()
+
+			cancelCh := make(chan struct{})
+			cancelGracefully := AtMostOnce(func() {
+				close(cancelCh)
+
+				Expect(cancel()).To(Succeed())
+				select {
+				case e := <-errs:
+					Expect(e).NotTo(HaveOccurred())
+				case <-time.After(terminateTimeout):
+					Fail(fmt.Sprintf("getenvoy command didn't exit gracefully within %s", terminateTimeout))
+				}
+			})
+			// make sure to stop Envoy if test fails
+			defer cancelGracefully()
+
+			// fail the test if `getenvoy extension run` exits with an error or unexpectedly
+			go func() {
+				select {
+				case e := <-errs:
+					Expect(e).NotTo(HaveOccurred(), "getenvoy command exited unexpectedly")
+				case <-cancelCh:
+				}
+			}()
+
+			stderrLines := e2e.StreamLines(se).Named("stderr")
+
+			By("waiting for Envoy Admin address to get logged")
+			adminAddressPattern := regexp.MustCompile(`admin address: ([^:]+:[0-9]+)`)
+			line, err := stderrLines.FirstMatch(adminAddressPattern).Wait(10 * time.Minute) // give time to compile the extension
+			Expect(err).NotTo(HaveOccurred())
+			adminAddress := adminAddressPattern.FindStringSubmatch(line)[1]
+
+			By("waiting for Envoy start-up to complete")
+			stderrLines.FirstMatch(regexp.MustCompile(`starting main dispatch loop`)).Wait(1 * time.Minute)
+
+			By("verifying Envoy is ready")
+			envoyClient, err := utilenvoy.NewClient(adminAddress)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(func() bool {
+				ready, e := envoyClient.IsReady()
+				return e == nil && ready
+			}, "60s", "100ms").Should(BeTrue())
+
+			By("verifying Wasm extensions have been created")
+			Eventually(func() bool {
+				stats, e := envoyClient.GetStats()
+				if e != nil {
+					return false
+				}
+				// at the moment, the only available Wasm metric is the number of Wasm VMs
+				concurrency := stats.GetMetric("server.concurrency")
+				activeWasmVms := stats.GetMetric("wasm.envoy.wasm.runtime.v8.active")
+				return concurrency != nil && activeWasmVms != nil && activeWasmVms.Value == concurrency.Value+2
+			}, "60s", "100ms").Should(BeTrue())
+
+			By("signaling Envoy to stop")
+			cancelGracefully()
 		},
 		testCases()...,
 	)
