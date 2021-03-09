93b40f69e66a7939bc42f29fbd79a8e29e24e7e0
Takeshi Yoneda <takeshi@tetrate.io>
2021-03-09 18:14:12 +0900

    improve access logger example: reading config
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- data/extension/init/templates/tinygo/envoy.access_loggers/default/main.go
+++ data/extension/init/templates/tinygo/envoy.access_loggers/default/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 )
 
 func main() {
@@ -11,6 +12,7 @@ func main() {
 type accessLogger struct {
 	// you must embed the default context
 	proxywasm.DefaultRootContext
+	logMessage string
 }
 
 func newAccessLogger(contextID uint32) proxywasm.RootContext {
@@ -18,12 +20,24 @@ func newAccessLogger(contextID uint32) proxywasm.RootContext {
 }
 
 // override
-func (ctx *accessLogger) OnLog() {
+func (l *accessLogger) OnPluginStart(configurationSize int) bool {
+	// Read plugin configuration provided in Envoy configuration
+	data, err := proxywasm.GetPluginConfiguration(configurationSize)
+	if err != nil && err != types.ErrorStatusNotFound {
+		proxywasm.LogCriticalf("failed to load config: %v", err)
+		return false
+	}
+	l.logMessage = string(data)
+	return true
+}
+
+// override
+func (l *accessLogger) OnLog() {
 	hdr, err := proxywasm.GetHttpRequestHeader(":path")
 	if err != nil {
 		proxywasm.LogCritical(err.Error())
 		return
 	}
 
-	proxywasm.LogInfof("OnLog: :path = %s", hdr)
+	proxywasm.LogInfof("OnLog: :path = %s && message = %s", hdr, l.logMessage)
 }
