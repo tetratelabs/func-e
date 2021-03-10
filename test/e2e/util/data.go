23e8fbc28d0e9cd0fef19322cb25859d463f5ef8
Takeshi Yoneda <takeshi@tetrate.io>
2021-03-10 16:53:28 +0900

    Revert: add go.sum
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- test/e2e/util/data.go
+++ test/e2e/util/data.go
@@ -37,6 +37,9 @@ func GetCategoryLanguageCombinations() []CategoryLanguageTuple {
 	tuples := make([]CategoryLanguageTuple, 0)
 	for _, category := range extension.Categories {
 		for _, language := range extension.Languages {
+			if language != "tinygo" {
+				continue
+			}
 			tuples = append(tuples, CategoryLanguageTuple{category, language})
 		}
 	}
