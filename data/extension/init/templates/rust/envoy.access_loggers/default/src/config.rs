f68fd7e994894db3c8c3f674eb6aa477886eb02a
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2021-04-19 16:03:17 +0800

    Makes rust examples use a smaller dependency for json (#185)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- data/extension/init/templates/rust/envoy.access_loggers/default/src/config.rs
+++ data/extension/init/templates/rust/envoy.access_loggers/default/src/config.rs
@@ -1,17 +1,18 @@
 use std::convert::TryFrom;
 
-use serde::Deserialize;
+use nanoserde::DeJson;
 
 use envoy::extension;
 
 /// Configuration for a Sample Access Logger.
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, DeJson)]
 pub struct SampleAccessLoggerConfig {}
 
 impl TryFrom<&[u8]> for SampleAccessLoggerConfig {
     type Error = extension::Error;
 
     fn try_from(value: &[u8]) -> extension::Result<Self> {
-        serde_json::from_slice(value).map_err(extension::Error::from)
+        let json = String::from_utf8(value.to_vec())?;
+        DeJson::deserialize_json(&json).map_err(extension::Error::from)
     }
 }
