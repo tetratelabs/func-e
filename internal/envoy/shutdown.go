218a777c7a310b5f79c782f7b9908d2e4a2dc1a4
Adrian Cole <adrian@tetrate.io>
2021-07-11 12:03:38 +0800

    comment
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- internal/envoy/shutdown.go
+++ internal/envoy/shutdown.go
@@ -65,6 +65,7 @@ func (r *Runtime) interruptEnvoy() {
 }
 
 func (r *Runtime) archiveRunDir() error {
+	// Ensure logs are closed before we try to archive them, particularly important in Windows.
 	if r.OutFile != nil {
 		r.OutFile.Close() //nolint
 	}
