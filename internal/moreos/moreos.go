2b76f96987b2b3851686e2a5325f8457d9220baf
Adrian Cole <adrian@tetrate.io>
2021-08-06 10:02:23 +1000

    Refactors release end-to-end tests to include Windows
    Before, we didn't test Windows. This adds tests and also makes updating
    the homebrew formula depend on e2e succeeding.
    
    Moreover, this removes a shell script in favor of `gh` commands which
    are included in the default images.
    
    See https://github.com/actions/virtual-environments/blob/main/images
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- internal/moreos/moreos.go
+++ internal/moreos/moreos.go
@@ -26,6 +26,7 @@ import (
 
 const (
 	// Exe is the runtime.GOOS-specific suffix for executables. Ex. "" unless windows which is ".exe"
+	// See https://github.com/golang/go/issues/47567 for formalization
 	Exe = exe
 	// OSDarwin is a Platform.OS a.k.a. "macOS"
 	OSDarwin = "darwin"
