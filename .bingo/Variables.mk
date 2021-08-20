17375b49a05c4a96f82937275ed0be5aa0dbf033
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2021-08-20 16:54:09 +0800

    Updates to Go 1.17 (#346)
    This only updates libraries to latest, it doesn't yet change anything to
    use new features or optimizations.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -25,11 +25,11 @@ $(GOIMPORTS): $(BINGO_DIR)/goimports.mod
 	@echo "(re)installing $(GOBIN)/goimports-v0.1.5"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goimports.mod -o=$(GOBIN)/goimports-v0.1.5 "golang.org/x/tools/cmd/goimports"
 
-GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.41.1
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.42.0
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/golangci-lint-v1.41.1"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.41.1 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.42.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.42.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
 GORELEASER := $(GOBIN)/goreleaser-v0.174.2
 $(GORELEASER): $(BINGO_DIR)/goreleaser.mod
