4f0dd5ee728acd373323f600f8e3da30466f6813
Dhi Aurrahman <dio@rockybars.com>
2021-09-11 01:06:45 -0700

    License
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- internal/test/func-e.go
+++ internal/test/func-e.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package test
 
 import (
@@ -16,7 +30,7 @@ var (
 	fakeFuncESrcPath = filepath.Join(funcEGoModuleDir, "internal", "test", "testdata", "fake_func-e", "fake_func-e.go")
 )
 
-// RequireFakeEnvoy writes fakeEnvoyBin to the given path
+// RequireFakeFuncE writes fakeFuncEBin to the given path.
 func RequireFakeFuncE(t *testing.T, path string) {
 	builtFakeFuncE.Do(func() {
 		fakeFuncEBin = requireBuildFakeFuncE(t)
