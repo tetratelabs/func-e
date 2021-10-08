8618494a864664607118814f31cf595fb79f2848
Clavin June <juneardoc@gmail.com>
2021-10-08 09:38:19 +0700

    add IsDebug test cases
    Signed-off-by: Clavin June <juneardoc@gmail.com>

---
+++ internal/version/version_test.go
@@ -0,0 +1,50 @@
+// Copyright 2021 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVersion_IsDebug(t *testing.T) {
+	tests := []struct {
+		Input    Version
+		Expected bool
+	}{
+		{
+			Input:    "1.19",
+			Expected: false,
+		},
+		{
+			Input:    "1.19.1",
+			Expected: false,
+		},
+		{
+			Input:    "1.19_debug",
+			Expected: true,
+		},
+		{
+			Input:    "1.19.1_debug",
+			Expected: true,
+		},
+	}
+
+	for _, tc := range tests {
+		actual := tc.Input.IsDebug()
+		require.Equal(t, tc.Expected, actual)
+	}
+}
