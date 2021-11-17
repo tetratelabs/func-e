a834b854f7321995aa70cec1eef03948cc1659ad
Lam Tran <tranngoclam288@gmail.com>
2021-11-17 15:33:29 +0700

    Revert the version of golangci-lint
    Signed-off-by: Lam Tran <tranngoclam288@gmail.com>

--- Tools.mk
+++ Tools.mk
@@ -1,7 +1,7 @@
 # Copyright 2021 Tetrate
 # Licensed under the Apache License, Version 2.0 (the "License")
 
-golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
+golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
 goimports     := golang.org/x/tools/cmd/goimports@v0.1.7
 hugo          := github.com/gohugoio/hugo@v0.89.3
 licenser      := github.com/liamawhite/licenser@v0.6.0
