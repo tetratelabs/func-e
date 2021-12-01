d08a2f0775ea53aa084f77ce2a555f7e39a67cef
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2021-12-02 07:17:47 +0800

    Updates to latest versions (#417)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- Tools.mk
+++ Tools.mk
@@ -3,6 +3,6 @@
 
 golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
 goimports     := golang.org/x/tools/cmd/goimports@v0.1.7
-hugo          := github.com/gohugoio/hugo@v0.89.3
+hugo          := github.com/gohugoio/hugo@v0.89.4
 licenser      := github.com/liamawhite/licenser@v0.6.0
-nfpm          := github.com/goreleaser/nfpm/v2/cmd/nfpm@v2.9.2
+nfpm          := github.com/goreleaser/nfpm/v2/cmd/nfpm@v2.10.0
