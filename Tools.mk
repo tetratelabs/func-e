821dbf1bc13b61a1dca02baea153fe9f49c197be
Adrian Cole <adrian@tetrate.io>
2022-01-05 13:11:59 +0800

    Bumps versions to latest
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- Tools.mk
+++ Tools.mk
@@ -2,7 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License")
 
 golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
-goimports     := golang.org/x/tools/cmd/goimports@v0.1.7
-hugo          := github.com/gohugoio/hugo@v0.89.4
+goimports     := golang.org/x/tools/cmd/goimports@v0.1.8
+hugo          := github.com/gohugoio/hugo@v0.91.2
 licenser      := github.com/liamawhite/licenser@v0.6.0
-nfpm          := github.com/goreleaser/nfpm/v2/cmd/nfpm@v2.10.0
+nfpm          := github.com/goreleaser/nfpm/v2/cmd/nfpm@v2.11.3
