d7ab36a9914939b8034434d5612c47245a49e035
Adrian Cole <adrian@tetrate.io>
2022-05-28 10:19:08 -0700

    Updates to use GitHub Action for codecov
    The codecov GitHub Action replaces the uploader script for projects that
    are either mostly amd64 or can consolidate uploads into one stage that
    runs on amd64. func-e has no significant arm64 source files, so can use
    this action instead of the uploader script.
    
    Thanks to @anuraaga for noticing the uploader script is deprecated.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- Tools.mk
+++ Tools.mk
@@ -1,9 +1,9 @@
 # Copyright 2021 Tetrate
 # Licensed under the Apache License, Version 2.0 (the "License")
 
-golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.45.2
+golangci_lint := github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2
 goimports     := golang.org/x/tools/cmd/goimports@v0.1.10
 # sync this with netlify.toml!
-hugo          := github.com/gohugoio/hugo@v0.98.0
+hugo          := github.com/gohugoio/hugo@v0.99.1
 licenser      := github.com/liamawhite/licenser@v0.6.0
 nfpm          := github.com/goreleaser/nfpm/v2/cmd/nfpm@v2.15.1
