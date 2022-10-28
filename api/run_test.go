3c3db2ba7cf1166082efd653953dbd5aa74cae57
Arko Dasgupta <arko@tetrate.io>
2022-10-28 15:33:12 -0700

    tests
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

---
+++ api/run_test.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+
+	"github.com/stretchr/testify/require"
+
+	"testing"
+)
+
+var (
+	// minRunArgs is the minimal config needed to run Envoy 1.18+, non-windows <1.18 need access_log_path: '/dev/stdout'
+	minRunArgs = []string{"--config-yaml", "admin: {address: {socket_address: {address: '127.0.0.1', port_value: 0}}}"}
+)
+
+func TestRunWithHomeDir(t *testing.T) {
+	tmpDir := t.TempDir()
+	require.NoError(t, Run(minRunArgs, HomeDir(tmpDir)))
+	_, err := os.Stat(filepath.Join(tmpDir, "versions"))
+	require.NoError(t, err)
+}
+
+func TestRunWithOut(t *testing.T) {
+	tmpDir := t.TempDir()
+	b := bytes.NewBufferString("")
+	require.Equal(t, 0, b.Len())
+	require.NoError(t, Run(minRunArgs, Out(b), HomeDir(tmpDir)))
+	require.NotEqual(t, 0, b.Len())
+}
