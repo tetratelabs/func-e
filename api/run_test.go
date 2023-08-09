8b44356a4ff9c310c5e46107e85d53f99e3f9086
Arko Dasgupta <arko@tetrate.io>
2023-08-08 19:50:15 -0700

    Allow downstream projects to import func-e
    * Define an api that allows go projects to use func-e as a library
    
    Takes https://github.com/tetratelabs/func-e/pull/434 forward
    
    Fixes: https://github.com/tetratelabs/func-e/issues/433
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>

---
+++ api/run_test.go
@@ -0,0 +1,55 @@
+// Copyright 2022 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/tetratelabs/func-e/internal/test"
+	"github.com/tetratelabs/func-e/internal/version"
+)
+
+var (
+	runArgs = []string{"--version"}
+)
+
+func TestRun(t *testing.T) {
+
+	tmpDir := t.TempDir()
+	envoyVersion := version.LastKnownEnvoy
+	versionsServer := test.RequireEnvoyVersionsTestServer(t, envoyVersion)
+	defer versionsServer.Close()
+	envoyVersionsURL := versionsServer.URL + "/envoy-versions.json"
+	b := bytes.NewBufferString("")
+
+	require.Equal(t, 0, b.Len())
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+	err := Run(ctx, runArgs, Out(b), HomeDir(tmpDir), EnvoyVersionsURL(envoyVersionsURL))
+	require.NoError(t, err)
+
+	require.NotEqual(t, 0, b.Len())
+	_, err = os.Stat(filepath.Join(tmpDir, "versions"))
+	require.NoError(t, err)
+}
