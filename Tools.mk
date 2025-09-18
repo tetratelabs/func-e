5d8785983254b159b2f3f63cff40b29815166b30
Adrian Cole <adrian@tetrate.io>
2025-09-18 13:55:59 +1000

    revert hugo
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- Tools.mk
+++ Tools.mk
@@ -9,6 +9,6 @@ golangci_lint := github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.4.0
 gofumpt       := mvdan.cc/gofumpt@v0.9.1
 gosimports    := github.com/rinchsan/gosimports/cmd/gosimports@v0.3.8
 # sync this with netlify.toml!
-hugo          := github.com/gohugoio/hugo@v0.150.0
+hugo          := github.com/gohugoio/hugo@v0.148.1
 nwa           := github.com/B1NARY-GR0UP/nwa@v0.7.5
 nfpm          := github.com/goreleaser/nfpm/v2/cmd/nfpm@v2.43.1
