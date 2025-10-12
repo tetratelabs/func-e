8d3327376ea456eb1c141c50c56ea668cbbce507
Adrian Cole <adrian@tetrate.io>
2025-10-11 22:17:01 -0400

    polish
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- internal/run/run.go
+++ internal/run/run.go
@@ -8,15 +8,15 @@ import (
 	"os"
 
 	"github.com/tetratelabs/func-e/api"
-	api2 "github.com/tetratelabs/func-e/internal/api"
+	internalapi "github.com/tetratelabs/func-e/internal/api"
 	"github.com/tetratelabs/func-e/internal/globals"
-	internalapi "github.com/tetratelabs/func-e/internal/runtime"
+	"github.com/tetratelabs/func-e/internal/runtime"
 	"github.com/tetratelabs/func-e/internal/version"
 )
 
 // EnvoyPath overrides the path to the Envoy binary. Used for testing with a fake binary.
 func EnvoyPath(envoyPath string) api.RunOption {
-	return func(o *api2.RunOpts) {
+	return func(o *internalapi.RunOpts) {
 		o.EnvoyPath = envoyPath
 	}
 }
@@ -25,7 +25,7 @@ func EnvoyPath(envoyPath string) api.RunOption {
 func Run(ctx context.Context, args []string, options ...api.RunOption) error {
 	// Check if middleware is set in context
 	baseRun := api.RunFunc(runImpl)
-	if middlewareVal := ctx.Value(api2.RunMiddlewareKey{}); middlewareVal != nil {
+	if middlewareVal := ctx.Value(internalapi.RunMiddlewareKey{}); middlewareVal != nil {
 		// Type assert to function that matches our middleware signature
 		if middleware, ok := middlewareVal.(func(api.RunFunc) api.RunFunc); ok {
 			baseRun = middleware(baseRun)
@@ -41,11 +41,11 @@ func runImpl(ctx context.Context, args []string, options ...api.RunOption) error
 	if err != nil {
 		return err
 	}
-	return internalapi.Run(ctx, o, args)
+	return runtime.Run(ctx, o, args)
 }
 
 func initOpts(ctx context.Context, options ...api.RunOption) (*globals.GlobalOpts, error) {
-	ro := &api2.RunOpts{
+	ro := &internalapi.RunOpts{
 		Out:      os.Stdout,
 		EnvoyOut: os.Stdout,
 		EnvoyErr: os.Stderr,
@@ -64,11 +64,11 @@ func initOpts(ctx context.Context, options ...api.RunOption) (*globals.GlobalOpt
 			StartupHook: ro.StartupHook,
 		},
 	}
-	if err := internalapi.InitializeGlobalOpts(o, ro.EnvoyVersionsURL, ro.HomeDir, ""); err != nil {
+	if err := runtime.InitializeGlobalOpts(o, ro.EnvoyVersionsURL, ro.HomeDir, ""); err != nil {
 		return nil, err
 	}
 
-	if err := internalapi.EnsureEnvoyVersion(ctx, o); err != nil {
+	if err := runtime.EnsureEnvoyVersion(ctx, o); err != nil {
 		return nil, err
 	}
 	return o, nil
